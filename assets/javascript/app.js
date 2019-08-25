13cf6b6035257471a9e8c3a2ca92486c22bd2ecc
YI YING <supercarboy88@gmail.com>
2019-08-24 23:47:01 -0700

    number list the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,27 +11,27 @@ var questions = [{
     answers:[" Seahawks ", " Marines ", " Sonics ", " Sounders FC "],
     correctAnswer:" Sonics ",
 },  {
-    question:"What venue was once home for both the Mariners and the Seahawks?",
+    question:"2. What venue was once home for both the Mariners and the Seahawks?",
     answers:[" Kingdome ", " CenturyLink Field ", " KeyArena ", " T-Mobile Park "],
     correctAnswer:" Kingdome ",
 },  {
-    question:"Where does the University of Washington football team play its home games?",
+    question:"3. Where does the University of Washington football team play its home games?",
     answers:[" Kingdome ", " CenturyLink Field ", " T-Mobile Park ", " Husky Stadium "],
     correctAnswer:" Husky Stadium ",
 }, {
-    question:"Who of the following played college basketball for the Washington Huskies?",
+    question:"4. Who of the following played college basketball for the Washington Huskies?",
     answers:[" John Stockton ", " Marques Johnson ", " Harold Miner ", " Detlef Schrempf "],
     correctAnswer:" Detlef Schrempf ",
 }, {
-    question:"Who of the following was NEVER head coach of the Seattle Seahawks?",
+    question:"5. Who of the following was NEVER head coach of the Seattle Seahawks?",
     answers:[" Tom Flores ", " Chuck Knox ", " Jack Patera ", " Jim Lambright "],
     correctAnswer:" Jim Lambright ",
 }, {
-    question:"What number did former Seahawk star wide receiver Steve Largent wear?",
+    question:"6. What number did former Seahawk star wide receiver Steve Largent wear?",
     answers:[" 78 ", " 80 ", " 82 ", " 85 "],
     correctAnswer:" 80 ",
 }, {
-    question:"Who of the following was NEVER a member of the Seattle Supersonics?",
+    question:"7. Who of the following was NEVER a member of the Seattle Supersonics?",
     answers:[" Patrick Ewing ", " Dennis Johnson ", " Bill Cartwright ", " Clyde Drexler "],
     correctAnswer:" Clyde Drexler ",
 }];
