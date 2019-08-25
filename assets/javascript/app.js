6dd5643a612f092d7aa73556bf3b7a17a0a7fd76
YI YING <supercarboy88@gmail.com>
2019-08-24 23:45:15 -0700

    add bg and other styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,33 +7,33 @@ $(document).on('click', '#end', function(){
 })
 
 var questions = [{
-    question:"What was the first professional organization in sports (NFL, NHL, NBA, MLB) for the city of Seattle?",
-    answers:["Seahawks", "Marines", "Sonics", "Sounders FC"],
-    correctAnswer:"Sonics",
+    question:"1. What was the first professional organization in sports (NFL, NHL, NBA, MLB) for the city of Seattle?",
+    answers:[" Seahawks ", " Marines ", " Sonics ", " Sounders FC "],
+    correctAnswer:" Sonics ",
 },  {
     question:"What venue was once home for both the Mariners and the Seahawks?",
-    answers:["Kingdome", "CenturyLink Field", "KeyArena", "T-Mobile Park"],
-    correctAnswer:"Kingdome",
+    answers:[" Kingdome ", " CenturyLink Field ", " KeyArena ", " T-Mobile Park "],
+    correctAnswer:" Kingdome ",
 },  {
     question:"Where does the University of Washington football team play its home games?",
-    answers:["Kingdome", "CenturyLink Field", "T-Mobile Park", "Husky Stadium"],
-    correctAnswer:"Husky Stadium",
+    answers:[" Kingdome ", " CenturyLink Field ", " T-Mobile Park ", " Husky Stadium "],
+    correctAnswer:" Husky Stadium ",
 }, {
     question:"Who of the following played college basketball for the Washington Huskies?",
     answers:[" John Stockton ", " Marques Johnson ", " Harold Miner ", " Detlef Schrempf "],
-    correctAnswer:"Detlef Schrempf",
+    correctAnswer:" Detlef Schrempf ",
 }, {
     question:"Who of the following was NEVER head coach of the Seattle Seahawks?",
-    answers:["Tom Flores", "Chuck Knox", "Jack Patera", "Jim Lambright"],
-    correctAnswer:"Jim Lambright",
+    answers:[" Tom Flores ", " Chuck Knox ", " Jack Patera ", " Jim Lambright "],
+    correctAnswer:" Jim Lambright ",
 }, {
     question:"What number did former Seahawk star wide receiver Steve Largent wear?",
-    answers:["78", "80", "82", "85"],
-    correctAnswer:"80",
+    answers:[" 78 ", " 80 ", " 82 ", " 85 "],
+    correctAnswer:" 80 ",
 }, {
     question:"Who of the following was NEVER a member of the Seattle Supersonics?",
-    answers:["Patrick Ewing", "Dennis Johnson", "Bill Cartwright", "Clyde Drexler"],
-    correctAnswer:"Clyde Drexler",
+    answers:[" Patrick Ewing ", " Dennis Johnson ", " Bill Cartwright ", " Clyde Drexler "],
+    correctAnswer:" Clyde Drexler ",
 }];
 
 var correct = 0;
@@ -54,12 +54,12 @@ function startGame (){
     $('#subwrapper').prepend('<h2>Time Remaining: <span id = "counter">24</span> Seconds</h2)');
     $('#start-game').remove();
     for (var i = 0; i < questions.length; i++){
-        $('#subwrapper').append('<h2>' + questions[i].question + '</h2>');
+        $('#subwrapper').append('</br><h2>' + questions[i].question + '</h2>');
         for (var j = 0; j <questions[i].answers.length; j++){
-            $("#subwrapper").append("<input type = 'radio' name = 'question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+            $("#subwrapper").append("</br><input type = 'radio' name = 'question-" + i + "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
         }
     }
-    $('#subwrapper').append('<br><button id = "end">DONE</button>');
+    $('#subwrapper').append('<br><button class="btn btn-success btn-block reset-button" id = "end">DONE</button>');
 }
 
 function done(){
